c232e29908a180b22116a2eeaf65bcb35d493835
takuro-oki-epkotsoftware <takuro.oki@epkotsoftware.co.jp>
2022-11-24 13:08:52 +0900

    11月24日データベース：シーディング前

--- template-laravel9-jp-main/template-laravel9-jp-main/root/database/factories/JobFactory.php
+++ template-laravel9-jp-main/template-laravel9-jp-main/root/database/factories/JobFactory.php
@@ -3,21 +3,23 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
  */
 class JobFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    private int $i = 1;
+
     public function definition()
     {
         return [
-            //
+            'name' => sprintf('JOB_%04d', $this->i++),
+            'deleted_at' => null,
+            'created_at' => '2021-12-30 11:22:33',
+            'updated_at' => '2021-12-31 23:58:59',
         ];
     }
 }
+
--- template-laravel9-jp-main/template-laravel9-jp-main/root/database/seeders/JobSeeder.php
+++ template-laravel9-jp-main/template-laravel9-jp-main/root/database/seeders/JobSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Database\Factories\JobFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -10,10 +11,19 @@ class JobSeeder extends Seeder
     /**
      * Run the database seeds.
      *
-     * @return void
+     * 
      */
     public function run()
     {
-        //
+        // 開発環境のみ100レコードを追加する。
+        if (app()->isLocal()) {
+            // App\Models\Job
+            // 全件削除
+            Job::truncate();
+            // JobFactoryクラスを使って100件追加
+            Job::factory()
+                ->count(100)
+                ->create();
+        }
     }
 }
